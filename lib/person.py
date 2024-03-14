087f917bf85de12d8add4623cfb86cb55a7ca432
Nadir0glu <nadiroglunail@gmail.com>
2024-03-13 20:32:05 -0400

    Nail

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "guido van rossum", job="Benevolent dictator for life"):
+        self.name = name
+        self.job = job
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 1 <= len(new_name) <= 25:
+            self._name = new_name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job #yooo get my job
+
+    @job.setter
+    def job(self, new_job): #you know what fuck it set me a new job with the given get job
+        if new_job in APPROVED_JOBS:
+            self._job = new_job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    
